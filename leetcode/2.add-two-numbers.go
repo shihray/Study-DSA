05ea023f4b7eab6a00b44a1f2ca8a7c51989cee4
shihray <diskahoy@gmail.com>
2022-08-16 14:23:19 +0800

    today's study

---
+++ leetcode/2.add-two-numbers.go
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=2 lang=golang
+ *
+ * [2] Add Two Numbers
+ */
+package leetcode
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+    carry := 0
+	dummy := &ListNode{}
+	cur := dummy
+	for l1 != nil || l2 != nil || carry != 0 {
+		sum := carry
+		
+		if l1 != nil {
+			sum += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			sum += l2.Val
+			l2 = l2.Next
+		}
+
+		carry = sum / 10
+		cur.Next = &ListNode{Val: sum%10}
+		cur = cur.Next
+	}
+
+	return dummy.Next
+}
+// @lc code=end
+
---
+++ leetcode/430.flatten-a-multilevel-doubly-linked-list.go
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=430 lang=golang
+ *
+ * [430] Flatten a Multilevel Doubly Linked List
+ */
+
+package leetcode
+
+type Node struct {
+    Val int
+    Prev *Node
+    Next *Node
+    Child *Node
+}
+// @lc code=start
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Prev *Node
+ *     Next *Node
+ *     Child *Node
+ * }
+ */
+
+func flatten(root *Node) *Node {
+    
+}
+// @lc code=end
+
