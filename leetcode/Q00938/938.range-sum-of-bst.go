2df3854b83532b00c533ff31eaa80d69c35359fa
shihray <diskahoy@gmail.com>
2022-12-07 09:42:54 +0800

    today's studay

---
+++ leetcode/Q00938/938.range-sum-of-bst.go
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=938 lang=golang
+ *
+ * [938] Range Sum of BST
+ */
+package q00938
+
+type TreeNode struct {
+    Val int
+    Left *TreeNode
+    Right *TreeNode
+}
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func rangeSumBST(root *TreeNode, low int, high int) int {
+    if root == nil {
+		return 0
+	}
+	if root.Val > high {
+		return rangeSumBST(root.Left, low, high)
+	}
+	if root.Val < low {
+		return rangeSumBST(root.Right, low, high)
+	}
+	return root.Val + rangeSumBST(root.Left, low, high) + rangeSumBST(root.Right, low, high)
+}
+
+// @lc code=end
+
