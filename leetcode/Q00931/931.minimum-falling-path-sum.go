955c6da627512eb1593dcee803d7f4be93560f0f
shihray <diskahoy@gmail.com>
2022-12-13 10:34:34 +0800

    dialy

---
+++ leetcode/Q00931/931.minimum-falling-path-sum.go
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode id=931 lang=golang
+ *
+ * [931] Minimum Falling Path Sum
+ */
+package q00931
+
+import (
+	"fmt"
+	"math"
+)
+
+// @lc code=start
+func minFallingPathSum(matrix [][]int) int {
+	tmp := map[string]int{}
+	resp := math.MaxInt32
+
+	for i := 0; i < len(matrix); i++ {
+		resp = min(resp, bfs(matrix, 0, i, tmp))
+	}
+    return resp
+}
+
+func bfs(arr [][]int, row, col int, val map[string]int) int {
+	if row >= len(arr) || row < 0 || col >= len(arr[0]) || col < 0 {
+		return math.MaxInt64
+	}
+
+	if row == len(arr[0]) - 1 {
+		return arr[row][col]
+	}
+
+	key := fmt.Sprintf("%d,%d", row, col)
+	if v, exist := val[key]; exist {
+		return v
+	}
+
+	aLeft := bfs(arr, row+1, col-1, val)
+	a := bfs(arr, row+1, col, val)
+	aRight := bfs(arr, row+1, col+1, val)
+
+	resp := min(min(a, aLeft), aRight) + arr[row][col]
+	val[key] = resp
+
+	return resp
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+// @lc code=end
+
